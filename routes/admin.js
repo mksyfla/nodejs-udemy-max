faa648226db108f4521c9047738920c3790f1da5
kasyfil <mksyfla@gmail.com>
2023-01-20 06:01:49 +0700

    section 5 - build program with express

---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+
+const rootDir = require('../utils/path');
+
+const router = express.Router();
+
+// /admin/add-product => method = GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+// /admin/add-product => method = POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
