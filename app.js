e9ddfef70880a130b0988c591a396c209274c1a8
kasyfil <mksyfla@gmail.com>
2023-01-21 19:11:11 +0700

    section 6 - add template engine (ejs)

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminRouter = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const rootDir = require('./utils/path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRouter);
-app.use(shopRouter);
+app.use('/admin', adminData.routes);
+app.use(shopRouter.routes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found',
+  });
 });
 
 app.listen(3000);
